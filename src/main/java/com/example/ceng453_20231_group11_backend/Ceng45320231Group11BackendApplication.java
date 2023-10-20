32504007345577d9041e66cfb6aedd6b909e57d0
ATINC UTKU ALPARSLAN <e2380061@ceng.metu.edu.tr>
2023-10-20 13:58:02 +0300

    spring boot init

---
+++ src/main/java/com/example/ceng453_20231_group11_backend/Ceng45320231Group11BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceng453_20231_group11_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng45320231Group11BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ceng45320231Group11BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ceng453_20231_group11_backend/Ceng45320231Group11BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceng453_20231_group11_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ceng45320231Group11BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
