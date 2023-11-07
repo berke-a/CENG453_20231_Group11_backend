c8fe08d02d66b8cdd83d1b4d434604680ea94f73
berke <berke.ahlatci@gmail.com>
2023-11-07 16:50:13 +0300

    create user mapper

---
+++ src/main/java/com/example/ceng453_20231_group11_backend/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.ceng453_20231_group11_backend.mapper;
+
+import com.example.ceng453_20231_group11_backend.dto.UserDTO;
+import com.example.ceng453_20231_group11_backend.entity.User;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface UserMapper {
+    List<UserDTO> toUserDTOList(List<User> userList);
+
+    User toUser(UserDTO userDTO);
+}
