3d32186f9405a31d48aab03b838af673a1fd85f5
berke <berke.ahlatci@gmail.com>
2023-11-07 16:58:55 +0300

    init user service

--- src/main/java/com/example/ceng453_20231_group11_backend/service/UserService.java
+++ src/main/java/com/example/ceng453_20231_group11_backend/service/UserService.java
@@ -1,4 +1,19 @@
 package com.example.ceng453_20231_group11_backend.service;
 
+import com.example.ceng453_20231_group11_backend.mapper.UserMapper;
+import com.example.ceng453_20231_group11_backend.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository,
+                       UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 }
