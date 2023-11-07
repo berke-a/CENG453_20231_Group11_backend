968777c1a3a7e7fa59e3adb76360488799046a70
berke <berke.ahlatci@gmail.com>
2023-11-07 16:23:49 +0300

    update score table

--- src/main/resources/db/init/create_score_table.sql
+++ src/main/resources/db/init/create_score_table.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Score
-(
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    username    VARCHAR(255) NOT NULL,
-    score_value INT          NOT NULL,
-    score_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (username) REFERENCES User (username)
-        ON DELETE CASCADE
+CREATE TABLE Score (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       player_id INT NOT NULL,
+                       score_value INT NOT NULL,
+                       created_at DATE NOT NULL,
+                       FOREIGN KEY (player_id) REFERENCES User(id)
+                           ON DELETE CASCADE
 );
