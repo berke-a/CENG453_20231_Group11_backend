125261b0ee3e1ab09f884517491d61962f73b2a6
berke <berke.ahlatci@gmail.com>
2023-11-09 14:29:44 +0300

    update user table

--- src/main/resources/db/init/create_user_table.sql
+++ src/main/resources/db/init/create_user_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE User
     id       INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(512)        NOT NULL,
-    email    VARCHAR(255) UNIQUE NOT NULL
+    email    VARCHAR(255) UNIQUE NOT NULL,
+    role     VARCHAR(255)        NOT NULL,
 );
