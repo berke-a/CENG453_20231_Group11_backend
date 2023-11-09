afb8713c7febbed2f6c219fe2655e7d609c35612
Berke Sina Ahlatcı <berke.ahlatci@gmail.com>
2023-11-09 15:16:28 +0300

    Sina/login endpoint (#2)
    * add security-core dependency
    
    * add findByUsername to repo
    
    * create handle for login in service
    
    * create login endpoint
    
    * refactor status constants

--- src/main/java/com/example/ceng453_20231_group11_backend/repository/UserRepository.java
+++ src/main/java/com/example/ceng453_20231_group11_backend/repository/UserRepository.java
@@ -5,6 +5,9 @@ import com.example.ceng453_20231_group11_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
