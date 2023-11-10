b315463e39417de97757a74fa64280210361f8d0
berke <berke.ahlatci@gmail.com>
2023-11-10 14:44:57 +0300

    update tostring for user dto

--- src/main/java/com/example/ceng453_20231_group11_backend/dto/UserDTO.java
+++ src/main/java/com/example/ceng453_20231_group11_backend/dto/UserDTO.java
@@ -11,4 +11,9 @@ public class UserDTO {
     private Long id;
     private String username;
     private String password;
+
+    @Override
+    public String toString() {
+        return String.format("UserDTO with id %d and username %s and password %s", id, username, password);
+    }
 }
