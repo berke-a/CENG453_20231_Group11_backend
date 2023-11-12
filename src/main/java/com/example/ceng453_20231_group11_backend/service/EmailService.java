997c8292f1d65f1d96f36bfb9ed051837356de04
Atinc Utku Alparslan <89345720+AtincUtku@users.noreply.github.com>
2023-11-12 21:20:30 +0300

    Refactor from email address

--- src/main/java/com/example/ceng453_20231_group11_backend/service/EmailService.java
+++ src/main/java/com/example/ceng453_20231_group11_backend/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.ceng453_20231_group11_backend.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,13 +13,16 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmailAddress;
+
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void sendPasswordResetToken(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ceng453group11catan@gmail.com");
+        message.setFrom(fromEmailAddress);
         message.setTo(to);
         message.setSubject("Password Reset Token");
         message.setText("Here is your password reset token: " + token +
