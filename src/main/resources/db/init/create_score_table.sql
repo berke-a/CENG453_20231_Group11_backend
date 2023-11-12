4588e4fb118e4e3346210054bc9913e2d26b4fd5
berke <berke.ahlatci@gmail.com>
2023-11-12 15:33:15 +0300

    update init table scripts

--- src/main/resources/db/init/create_score_table.sql
+++ src/main/resources/db/init/create_score_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Score
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
-    user_id   INT  NOT NULL,
+    user_id   BIGINT  NOT NULL,
     score_value INT  NOT NULL,
     created_at  TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES User (id)
